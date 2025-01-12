f6d142e7768d9b187de44bd81cd6837b2948c797
galthran-wq <levmorozov900@gmail.com>
2025-01-12 13:58:20 +0000

    fix hello world example

--- src/dags/hello_world.py
+++ src/dags/hello_world.py
@@ -1,28 +1,32 @@
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 import os
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.models.param import Param
+
 import zipfile
 import pandas as pd
 
-def print_hello():
-    return 'Hello, World!'
-
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
-    'retries': 1,
-}
+def print_hello(name: str):
+    return f'Hello, World! Hello, {name}'
 
 dag = DAG(
-    'print_hello',
-    default_args=default_args,
-    schedule_interval='@once',
+    dag_id='print_hello',
+    schedule=None,
+    schedule_interval=None,
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    params={
+        "name": Param("example", type="string", description="name to greet")
+    },
 )
 
 print_hello = PythonOperator(
     task_id='print_hello',
     python_callable=print_hello,
+    op_args=["{{ params.name }}"],
+    # params={"name": Param(type="string", description="name to greet")},
     dag=dag,
 )
 
